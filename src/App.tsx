a1ed5713e44246ba721569553aba22e8a2a65b90
fmlizarraga <lizarragafranco91@gmail.com>
2024-07-06 13:22:42 -0300

    skeleton project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from './pages/Home.tsx'
+
+function LetsCookApp() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Home />
+      </main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default LetsCookApp
