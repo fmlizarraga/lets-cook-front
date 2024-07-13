465b221aecf389493eb9482fbafc2406a3ec12b1
fmlizarraga <lizarragafranco91@gmail.com>
2024-07-13 02:06:30 -0300

    moved components from App to Home layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { Footer, Header, LoaderStatus } from "./components";
 import AppRouter from "./router/AppRouter";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { BlogRepositoryProvider } from "./repository";
+import { AuthRepositoryProvider, BlogRepositoryProvider } from "./repository";
+import LocalAuthRepository from "./repository/LocalAuthRepository";
 import LocalBlogRepository from "./repository/LocalBlogRepository";
 
 import 'primereact/resources/themes/lara-dark-amber/theme.css';
 import 'primeicons/primeicons.css';
 
 function LetsCookApp() {
-  const blogRepository = LocalBlogRepository;
-  return (
-    <>
-      <Provider store={store} >
-        <BlogRepositoryProvider repository={blogRepository}>
-          <header>
-            <Header />
-          </header>
-          <main>
-            <LoaderStatus />
-            <AppRouter />
-          </main>
-          <footer>
-            <Footer />
-          </footer>
-        </BlogRepositoryProvider>
-      </Provider>
-    </>
-  )
-}
+    const authRepository = LocalAuthRepository;
+    const blogRepository = LocalBlogRepository;
+    return (
+        <>
+            <Provider store={store}>
+                <AuthRepositoryProvider repository={authRepository}>
+                    <BlogRepositoryProvider repository={blogRepository}>
+                        <AppRouter/>
+                    </BlogRepositoryProvider>
+                </AuthRepositoryProvider>
+            </Provider>
+        </>
+    )
+};
 
 export default LetsCookApp;
--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,9 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Footer, Header, LoaderStatus } from "../components";
 
 export const Home = () => {
   return (
     <>
-        <Outlet />
+        <header>
+          <Header />
+        </header>
+        <main>
+          <LoaderStatus />
+          <Outlet />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
     </>
   )
 };
